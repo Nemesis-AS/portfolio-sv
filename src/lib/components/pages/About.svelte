2c6964f6797b11a5e2b1dc1e3027751b9acf4b9a
Akshat Shukla <shuklaakshatascom@gmail.com>
2024-06-03 00:18:38 +0530

    Optimized Images, edited about and styled scrollbar

--- src/lib/components/pages/About.svelte
+++ src/lib/components/pages/About.svelte
@@ -1,9 +1,17 @@
 <section id="about" class="p-4 font-display max-w-screen-lg mx-auto">
-    <div class="text-4xl font-bold py-6">About</div>
-    <div class="flex gap-4">
-        <div class="w-16 shrink-0 mt-4 h-1 bg-brand-golden"></div>
-        <div class="text-lg">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quae quia impedit soluta nobis. Laborum nam, pariatur nulla quo iusto dolor. Doloribus quo, officiis fugit quasi voluptatibus voluptatum nam non laborum! Quasi, sunt quisquam! Blanditiis, sunt delectus. Sequi autem beatae aspernatur quae esse, architecto rem reprehenderit tempora perspiciatis, soluta repellat doloribus, maiores deserunt necessitatibus a blanditiis provident tenetur magni praesentium sit? Veniam consequuntur in dolore qui! <span class="text-brand-golden">Nostrum earum nam beatae officia explicabo modi</span> ratione quis. Alias tempora voluptatibus aliquid numquam consectetur velit, possimus eveniet illo minima sit, blanditiis veritatis assumenda beatae laborum voluptate culpa quos sequi ipsum voluptatum repellat! Fugit.
-        </div>
-    </div>
+	<div class="text-4xl font-bold py-6">About</div>
+	<div class="flex gap-4">
+		<div class="w-16 shrink-0 mt-4 h-1 bg-brand-golden"></div>
+		<div class="text">
+			<p class="text-lg py-2">
+				Hey, I am Akshat Shukla. I am a fullstack web and game developer, specializing in the <span class="text-brand-golden">Godot Game Engine</span> for gamedev and the <span class="text-brand-golden">MERN</span> stack for webdev. I am also a member at <span class="text-brand-golden">Innogeeks</span>, <span class="text-brand-golden">NSCC</span>
+				and <span class="text-brand-golden">Technocrats</span> @KIET.
+			</p>
+
+			<p class="text-lg py-2">
+				I love a nice cup of coffee while working, and would love to discuss any ideas over coffee
+				:D
+			</p>
+		</div>
+	</div>
 </section>
